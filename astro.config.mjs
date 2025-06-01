7698c9d645199431acc26d9a8781919bb4a6eeef
rayhanmp <rayhanmpramanda@gmail.com>
2025-06-01 19:15:32 +0700

    feat(blog): add support for youtube embedding and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   alias: {
     '@': fileURLToPath(new URL('./src', import.meta.url))
   },
