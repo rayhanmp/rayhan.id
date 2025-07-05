0316947d3f7524bf95207ae71dd67027ec390eab
rayhanmp <rayhanmpramanda@gmail.com>
2025-07-05 10:15:33 +0700

    misc: add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import { fileURLToPath } from 'url';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
+  site: 'https://rayhan.id',
   alias: {
     '@': fileURLToPath(new URL('./src', import.meta.url))
   },
