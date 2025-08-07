9ff787d2809bdcdf0c80c4841c07ea85406dabf0
rayhanmp <rayhanmpramanda@gmail.com>
2025-08-07 15:30:52 +0700

    add support for copy paste in code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import { fileURLToPath } from 'url';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import { addCopyButton } from 'shiki-transformer-copy-button';
+
+const options = {
+  // delay time from "copied" state back to normal state
+  toggle: 3000
+}
 
 export default defineConfig({
   integrations: [react(), mdx(), sitemap()],
@@ -13,7 +21,10 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: 'github-dark'
-    }
+      theme: 'one-light',
+      transformers: [addCopyButton(options)]
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
   }
 });
