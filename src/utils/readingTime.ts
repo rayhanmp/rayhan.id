6b64299aad34739c852c78a45909ba93ae52cefc
rayhanmp <rayhanmpramanda@gmail.com>
2025-09-28 17:31:31 +0700

    fix(readingTime): update reading time calculation to use 200 words per minute

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -1,5 +1,5 @@
 export function getReadingTime(content: string): number {
     const words = content.trim().split(/\s+/).length;
-    return Math.ceil(words / 250);
-  }
+    return Math.ceil(words / 200);
+}
   
